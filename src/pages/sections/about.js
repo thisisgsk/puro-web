9957700725767e97c7f1e71faacd7564aa8ce3c1
Sai Karthik Godavarthy <karthik.cvr70@gmail.com>
2025-01-28 00:23:58 +0530

    Added use client for all animated sections and removed unused imports

--- src/pages/sections/about.js
+++ src/pages/sections/about.js
@@ -1,3 +1,4 @@
+"use client";
 import { useRef } from "react";
 import { Chip, Card, CardHeader, CardBody, ScrollShadow } from "@nextui-org/react";
 import { motion, useInView } from "framer-motion";
